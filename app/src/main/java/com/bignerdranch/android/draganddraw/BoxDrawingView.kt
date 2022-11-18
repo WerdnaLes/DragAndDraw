4f973d23f5f007ec03cb8a0077f31ecf7a9486de
WerdnaLes <werdnales@gmail.com>
2022-11-18 23:57:23 +0200

    Finished chap 24(Custom Views). Finished challenge:Saving State

--- app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.kt
@@ -2,11 +2,16 @@ package com.bignerdranch.android.draganddraw
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.graphics.PointF
+import android.os.Bundle
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import java.util.ArrayList
 
 private const val TAG = "BoxDrawingView"
 
@@ -15,27 +20,85 @@ class BoxDrawingView(
     attrs: AttributeSet? = null
 ) : View(context, attrs) {
 
-    @SuppressLint("ClickableViewAccessibility")
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        val current = event?.let { PointF(event.x, event.y) }
-        var action = ""
-        when (event?.action) {
-            MotionEvent.ACTION_DOWN -> {
-                action = "ACTION_DOWN"
-            }
-            MotionEvent.ACTION_MOVE -> {
-                action = "ACTION_MOVE"
-            }
-            MotionEvent.ACTION_UP -> {
-                action = "ACTION_UP"
-            }
-            MotionEvent.ACTION_CANCEL -> {
-                action = "ACTION_CANCEL"
+    private var currentBox: Box? = null
+    private var boxes = mutableListOf<Box>()
+    private val boxPaint = Paint().apply {
+        color = 0x22ff0000
+    }
+    private val backgroundPaint = Paint().apply {
+        color = 0xfff8efe0.toInt()
+    }
+
+
+    @Suppress("UNCHECKED_CAST")
+    override fun onSaveInstanceState(): Parcelable {
+        val bundle = Bundle()
+        bundle.putParcelable("superState", super.onSaveInstanceState())
+        bundle.putParcelableArrayList("boxes", boxes as ArrayList<out Parcelable>)
+
+        return bundle
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        var newState: Parcelable? = state
+        if (state is Bundle) {
+            val savedBoxes = state.getParcelableArrayList<Parcelable>("boxes")
+            boxes = savedBoxes as MutableList<Box>
+            newState = state.getParcelable("superState")
+        }
+        super.onRestoreInstanceState(newState)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.let {
+            canvas.drawPaint(backgroundPaint)
+
+            boxes.forEach { box ->
+                canvas.drawRect(box.left, box.top, box.right, box.bottom, boxPaint)
             }
         }
+    }
 
-        Log.i(TAG, "$action at x=${current?.x}")
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event != null) {
+            val current = PointF(event.x, event.y)
+            var action = ""
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    action = "ACTION_DOWN"
+                    // Reset drawing state:
+                    currentBox = Box(current).also {
+                        boxes.add(it)
+                    }
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    action = "ACTION_MOVE"
+                    updateCurrentBox(current)
+                }
+                MotionEvent.ACTION_UP -> {
+                    action = "ACTION_UP"
+                    updateCurrentBox(current)
+                    currentBox = null
+                }
+                MotionEvent.ACTION_CANCEL -> {
+                    action = "ACTION_CANCEL"
+                    currentBox = null
+                }
+            }
 
-        return true
+            Log.i(TAG, "$action at x=${current.x}")
+
+            return true
+        } else return false
+    }
+
+    private fun updateCurrentBox(current: PointF) {
+        currentBox?.let {
+            it.end = current
+            invalidate()
+        }
     }
 }
